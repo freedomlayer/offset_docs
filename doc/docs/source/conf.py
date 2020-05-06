99e25168fb35408c0b7c85c9d2f583bfaac483e7
real <real@freedomlayer.org>
2020-05-06 14:31:13 +0300

    Added logo + repository link

--- doc/docs/source/conf.py
+++ doc/docs/source/conf.py
@@ -47,11 +47,17 @@ exclude_patterns = []
 html_theme = 'alabaster'
 
 html_theme_options = {
-        'sidebar_width': '280px',
+    'sidebar_width': '280px',
+    'github_user': 'freedomlayer',
+    'github_repo': 'offset_docs',
+    'github_button': 'false',
+    'github_banner': 'true',
+    'logo': 'offset_logo_128_128.png',
+    'logo_name': 'true',
+    'logo_text_align': 'center',
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-html_static_path = []
+html_static_path = ['_static']
