091584eda8566787400db03a59380b8a4f285c9a
real <real@freedomlayer.org>
2020-05-17 19:30:58 +0300

    Disabled scaled image links

--- doc/docs/source/conf.py
+++ doc/docs/source/conf.py
@@ -70,6 +70,8 @@ html_sidebars = {
 
 }
 
+html_scaled_image_link = False
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
